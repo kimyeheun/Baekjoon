b67793af5463916ef1dd4d749731f864b0cae4ba
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-04-28 01:00:59 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc. nextInt();
+        System.out.println(pibo(T));
+    }
+    public static int pibo(int t) {
+        if (t < 2) return t;
+        else 
+            return pibo(t-1) + pibo(t-2);
+    }
+}
