cc9eaf22cd38770366dfea6b925d7e5834aab959
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-05-27 00:49:37 +0900

    [Gold V] Title: 빗물, Time: 276 ms, Memory: 18296 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+
+        Integer[] place = new Integer[w+1];
+
+        for(int i = 0; i < w; i++) {
+            place[i] = sc.nextInt();
+        }
+        int rain = 0;
+        for(int u = 1; u < place.length-2; u++) {
+            int left_max = 0;
+            int right_max = 0;
+            for(int y = 0; y < u; y++) {
+                if(left_max < place[y])
+                    left_max = place[y];
+            }
+            for(int e = u+1; e < place.length-1; e++) {
+                if(right_max < place[e])
+                    right_max = place[e];
+            }
+            int min = Math.min(left_max, right_max);
+
+            if(place[u] < min)
+                rain += min - place[u];
+        }
+        System.out.println(rain);
+    }
+}
