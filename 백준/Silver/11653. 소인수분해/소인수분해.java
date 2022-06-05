e3e7f765cfd80538aaba692df4d3c8b15edc90d8
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-06-05 17:31:53 +0900

    [Silver V] Title: 소인수분해, Time: 212 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Silver/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        
+		int num = in.nextInt();
+ 
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			while (num % i == 0) {
+				System.out.println(i);
+				num /= i;
+			}
+		}
+		if (num != 1) {
+			System.out.println(num);
+		}
+	}
+}
