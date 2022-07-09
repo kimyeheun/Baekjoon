f1e7d5e53a01f2a47bc32890566c7d67351c1066
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-07-09 08:33:47 -0700

    Create 1로 만들기.java

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int[] dp = new int[a+1];
+        
+        dp[1] = 0;
+        dp[0] = 0;
+        
+        for(int i = 2; i < a+1; i++) {
+            dp[i] = dp[i-1] + 1;
+            
+            if(i%2==0 & dp[i] > dp[i/2] +1) {
+                 dp[i] = dp[i/2] + 1;
+            }
+            if(i%3==0 & dp[i] > dp[i/3] +1) {
+                 dp[i] = dp[i/3] +1;
+            }
+        }
+        System.out.println(dp[a]);
+    }
+}
