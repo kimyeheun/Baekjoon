a989a11bde009205e80ba2dbce1fdd4e0c660b77
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-07-11 11:00:00 +0900

    Update 단어 정렬.java

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -8,16 +8,23 @@ public class Main {
 
         int word = sc.nextInt();
         List<String> list = new ArrayList<>();
+        
+        //nextInt() 쓴 다음에 nextLine()쓰면 다음에 "\n"이 저장된다. 따라서 한 줄을 버림.
         sc.nextLine();
+        
         for(int i = 0; i <word; i++) {
             list.add(sc.nextLine());
         }
+        
+        //중복 제거하여 정렬
         List<String> rlist = list.stream().distinct().collect(Collectors.toList());
         String[] nlist = new String[rlist.size()];
 
         for(int i = 0; i < rlist.size(); i++) {
             nlist[i] = rlist.get(i);
         }
+        
+        //오버라이딩하여 배열 정렬. 이때, 양수를 반환하면 위치를 바꾼다. 즉, 길이가 길면 뒤로 보내진다. 
         Arrays.sort(nlist, new Comparator<String>() {
             @Override
             public int compare (String s1, String s2) {
