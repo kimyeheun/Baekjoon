148c03bbd64e51c6a2efb70323146b5bdc05e910
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-09-01 19:25:28 +0900

    [level 1] Title: 체육복, Time: 0.48 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] student = new int[n];
+        Arrays.fill(student, 1);
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        for(int i = 0; i < lost.length; i++) {
+            student[lost[i]-1] = 0;
+        }
+        
+        int answer = 0;
+        
+        for(int u = 0; u < reserve.length; u++){
+            if(student[reserve[u] -1] == 0) {
+                    student[reserve[u] -1] = 1;
+                    reserve[u] = -1;
+            }
+        }
+        
+        
+        for(int i = 0; i < student.length; i++) {
+            if(student[i] == 0) {
+                for(int u = 0; u < reserve.length; u++) {
+                    if(reserve[u] == -1) 
+                        continue;
+                    else if(reserve[u] -2 == i || reserve[u] == i) {
+                        student[i] = 1;
+                        reserve[u] = -1;
+                        break;
+                    } 
+                }
+                
+                
+            }  
+        }
+        
+        for(int i = 0; i < student.length; i++) {
+            if(student[i] == 1)
+                answer++;
+        }
+        
+        
+        return answer;
+    }
+}
