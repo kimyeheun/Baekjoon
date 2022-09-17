7b29e604db188a9ed26041d6cb3bd092cf053460
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-09-17 22:37:03 +0900

    [Gold IV] Title: 치즈, Time: 176 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -40,7 +40,7 @@ public class Main {
             ans = ans_cheese;
             ans2 = ans_time;
             visit = new boolean[N][M];
-            out_air(air.peek()[0], air.peek()[1]);
+            out_air(air.peek()[0], air.poll()[1]);
             melt();
             ans_time++;
         }
@@ -78,6 +78,7 @@ public class Main {
                 if (next_x >= 0 && next_y >= 0 && next_x < N && next_y < M) {
                     if (!visit[next_x][next_y] && cheese[next_x][next_y] == 1) {
                         air.add(new int[]{next_x, next_y});
+                        visit[next_x][next_y] = true;
                         cheese[next_x][next_y] = 0;
                         ans_cheese++;
                     }
@@ -85,4 +86,27 @@ public class Main {
             }
         }
     }
+
+
 }
+
+/*
+[망해버린 방법]
+1. 치즈 입력
+2. 치즈 안에 공기 찾기...??
+3. cheese가 1이고 주위에 0이 있으면 11로 바꾼다. 하고 visit true로 바꾸기.
+4. 다시 반복문 돌려. visit이 false인 공간만 탐색. if 주의에 11이 있으면, 12로 바꾼다.
+5. 이 과정을 모두 visit 될 때 까지 반복.
+6. 배열에서 (가장 큰 값) - 10 = 치즈가 녹는 시간. / 가장 큰 값의 개수 = 치즈 개수.
+
+!. 모두 visit 처리 되었는지 = 치즈가 모두 녹았는지 확인하는 함수
+!.
+ */
+
+/*
+[??? : 큐에 치즈를 넣는거야?]
+1. 치즈 입력
+2. 밖에 0 을 모두 큐에 넣는다.
+3. 그 큐와 1이 맞닿아 있다면 녹임. (0으로 만들고 큐에 넣는다)
+큐에 들어있는 요소의 개수를 센다.
+ */
