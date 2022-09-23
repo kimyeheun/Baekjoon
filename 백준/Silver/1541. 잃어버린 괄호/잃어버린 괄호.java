e0d956d81c73e117e0e33c23d842636f1f015aca
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-09-23 12:18:55 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,13 +10,12 @@ public class Main {
         StringTokenizer st  =new StringTokenizer(line, "+|-", true);
 
         int[] minus = new int[line.length()];
-        int i = 1, j = 0;
+        int j = 0;
         int fir = Integer.parseInt(st.nextToken());
-        int sec = 0;
-        
+
         while(st.hasMoreTokens()) {
             String opera = st.nextToken();
-            sec = Integer.parseInt(st.nextToken());
+            int sec = Integer.parseInt(st.nextToken());
             if(opera.equals("+"))
                 fir = fir + sec;
             if(opera.equals("-")) {
@@ -29,7 +27,7 @@ public class Main {
         minus[j] = fir;
         int sum = minus[0];
 
-        for (i = 1; i < minus.length; i++) {
+        for (int i = 1; i < minus.length; i++) {
             sum -= minus[i];
         }
         System.out.println(sum);
