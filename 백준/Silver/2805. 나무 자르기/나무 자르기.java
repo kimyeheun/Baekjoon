fb9867abbb4d3a03bd5b969321ed0024a983aa24
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2022-09-26 02:08:09 +0900

    [Silver II] Title: 나무 자르기, Time: 1768 ms, Memory: 328316 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,40 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
+        int N = sc.nextInt();
+        long M = sc.nextLong(); //가져가는 나무의 길이가 M이랑 같거나 커도 됨.
+        long[] tree = new long[N];
 
-        int[] trees = new int[N + 1];
+        long min = 0;
         long max = 0;
-        
-        for (int i = 1; i <= N; i++) {
-            trees[i] = scanner.nextInt();
-            max = Math.max(max, trees[i]);
+
+        for (int n = 0; n < N; n++) {
+            tree[n] = sc.nextInt();
+            if(max < tree[n])
+                max = tree[n];
         }
 
-        long start = 0;
+        while(min <= max) {
+            long bring = 0;
+            long mid = (min + max) / 2;
 
-        while (start <= max) {
-            long mid = (start + max) / 2;
-            long sum = 0;
-
-            for (int tree : trees) {
-                if (tree > mid) {
-                    sum += tree - mid;
-                }
+            for (int n = 0; n < N; n++) {
+                if(tree[n] - mid > 0)
+                    bring += tree[n] - mid;
             }
 
-            if (sum >= M) {
-                start = mid + 1;
-            } else {
+            if(bring >= M) {
+                min = mid + 1;
+            }
+            else
                 max = mid - 1;
-            }
         }
-
         System.out.println(max);
     }
 }
