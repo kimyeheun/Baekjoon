8d72536cbea31c96aa4c0a4b1d5858711b77ecfb
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2023-01-20 10:03:18 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        long[] fibo = new long[n+1];
+
+        if (n < 2) {
+            System.out.println(n);
+            return;
+        }
+
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        System.out.println(fibo[n]);
+    }
+}
