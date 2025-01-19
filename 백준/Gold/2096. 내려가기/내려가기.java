9599f2a3644e2a5b17d38f5fcd375f8e24efc4fc
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2025-01-19 10:27:12 +0900

    [Gold V] Title: 내려가기, Time: 372 ms, Memory: 54988 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,43 +1,64 @@
+import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int N;
 
-        int floor = sc.nextInt();
+    static int[] Max;
+    static int[] Min;
 
-        int[] now = new int[3];
-        int[] maxp = new int[3];
-        int[] minp = new int[3];
+    static void calc(int[] matrix) {
+        int[] bufmax = new int[5];
+        int[] bufmin = new int[5];
+        int max, min;
 
-        maxp[0] = sc.nextInt();
-        maxp[1] = sc.nextInt();
-        maxp[2] = sc.nextInt();
+        for (int j = 1; j <= 3; j++) {
+            max = Math.max(Max[j-1], Max[j]);
+            max = Math.max(max, Max[j+1]);
 
-        minp[0] = maxp[0];
-        minp[1] = maxp[1];
-        minp[2] = maxp[2];
+            min = Math.min(Min[j-1], Min[j]);
+            min = Math.min(min, Min[j+1]);
 
-        for(int i = 1; i < floor; i++) {
-            now[0] = sc.nextInt();
-            now[1] = sc.nextInt();
-            now[2] = sc.nextInt();
-
-            int ax = maxp[0];
-            int bx = maxp[2];
-            maxp[0] = Math.max(maxp[0], maxp[1]) + now[0];
-            maxp[2] = Math.max(maxp[1], maxp[2]) + now[2];
-            maxp[1] = Math.max(Math.max(maxp[1], bx), ax) + now[1];
-
-            int an = minp[0];
-            int bn = minp[2];
-            minp[0] = Math.min(minp[0], minp[1]) + now[0];
-            minp[2] = Math.min(minp[1], minp[2]) + now[2];
-            minp[1] = Math.min(Math.min(minp[1], bn), an) + now[1];
+            bufmax[j] = max + matrix[j];
+            bufmin[j] = min + matrix[j];
         }
 
-        System.out.print(Math.max(Math.max(maxp[0], maxp[1]),maxp[2]));
-        System.out.print(" ");
-        System.out.println(Math.min(Math.min(minp[0], minp[1]),minp[2]));
+        Min = Arrays.copyOf(bufmin, bufmin.length);
+        Max = Arrays.copyOf(bufmax, bufmax.length);
+        Min[0] = Integer.MAX_VALUE;
+        Min[4] = Integer.MAX_VALUE;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        int[] matrix = new int[5];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= 3; j++) {
+                matrix[j] = Integer.parseInt(st.nextToken());
+            }
+
+            if (i == 0) {
+                Max = Arrays.copyOf(matrix, matrix.length);
+                Min = Arrays.copyOf(matrix, matrix.length);
+                Min[0] = Integer.MAX_VALUE;
+                Min[4] = Integer.MAX_VALUE;            
+            }
+            else calc(matrix);
+        }
+
+        int result_min = Integer.MAX_VALUE-1;
+        int result_max = 0;
+        for (int i = 1; i <= 3; i++) {
+            result_max = result_max < Max[i] ? Max[i] : result_max;
+            result_min = result_min > Min[i] ? Min[i] : result_min;
+        }
+
+        System.out.println(result_max + " " + result_min);
+
     }
 }
