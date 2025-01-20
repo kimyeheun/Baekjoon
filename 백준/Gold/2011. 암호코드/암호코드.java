22bdcbc2879c154f66902708b4342447e192f515
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2025-01-20 23:02:37 +0900

    [Gold V] Title: 암호코드, Time: 104 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.java
+++ 백준/Gold/2011. 암호코드/암호코드.java
@@ -18,20 +18,17 @@ public class Main {
         if (word.length() >= 2) {
             for (int i = 2; i <= word.length(); i++) {
                 long num = Long.parseLong(word.substring(i - 2, i));
-                if (num == 0) {
+                // 00 <- 거르기
+                if (num == 0 || (num % 10 == 0 && num >= 30)) {
                     System.out.println(0);
                     return;
                 }
-                if (num % 10 == 0) {
-                    if (num >= 30) {
-                        System.out.println(0);
-                        return;
-                    }
-                    else {
-                        dp[i] = dp[i-2] % 1000000;
-                    }
+                // 10, 20 이 나오면, dp[i-2]를 가져옴
+                if (num == 10 || num == 20) {
+                    dp[i] = dp[i - 2] % 1000000;
                     continue;
                 }
+
                 if (num > 26 || word.charAt(i-1) - '0' == 0 || word.charAt(i-2) - '0' == 0) {
                     dp[i] = dp[i - 1] % 1000000;
                 }
