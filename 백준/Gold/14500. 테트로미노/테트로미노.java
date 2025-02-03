3ca4e44208a6e3896085610b0dbbd643dd78e57e
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2025-02-03 22:10:31 +0900

    [Gold IV] Title: 테트로미노, Time: 468 ms, Memory: 41748 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,134 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[][] map;
+    static int result = 0;
+    static int[][] row3Block = {{-1,0},{-1,1},{-1,2},{0,3},{1,2},{1,1},{1,0},{0,-1}};
+    static int[][] col3Block = {{-1,0},{0,1},{1,1},{2,1},{3,0},{2,-1},{1,-1},{0,-1}};
+    static int[][] row2Block = {{-1,-1},{-1,0},{-1,1},{-1,2},
+                                {1,-1},{1,0},{1,1},{1,2}};
+    static int[][] col2Block = {{-1,1},{0,1},{1,1},{2,1},
+                                {2,-1},{1,-1},{0,-1},{-1,-1}};
+
+
+    static void playGame(int[] xy) {
+        int max = 0;
+        int rowBlock;
+        int colBlock;
+
+        // row - 2
+        if (xy[1] + 1 < M) {
+            rowBlock = map[xy[0]][xy[1]] + map[xy[0]][xy[1] + 1];
+
+            for (int i = 0; i < 3; i++) {
+                int buf_i = xy[0] + row2Block[i][0];
+                int buf_j = xy[1] + row2Block[i][1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                int buf_max = map[buf_i][buf_j];
+                buf_i = xy[0] + row2Block[i + 1][0];
+                buf_j = xy[1] + row2Block[i + 1][1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                buf_max += map[buf_i][buf_j];
+
+                max = Math.max(max, buf_max);
+            }
+            for (int i = 4; i < 7; i++) {
+                int buf_i = xy[0] + row2Block[i][0];
+                int buf_j = xy[1] + row2Block[i][1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                int buf_max = map[buf_i][buf_j];
+                buf_i = xy[0] + row2Block[i + 1][0];
+                buf_j = xy[1] + row2Block[i + 1][1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                buf_max += map[buf_i][buf_j];
+
+                max = Math.max(max, buf_max);
+            }
+            result = Math.max(result, rowBlock + max);
+        }
+
+        // col - 2
+        max = 0;
+        if (xy[0] + 1 < N) {
+            colBlock = map[xy[0]][xy[1]] + map[xy[0] + 1][xy[1]];
+
+            for (int i = 0; i < 3; i++) {
+                int buf_i = xy[0] + col2Block[i][0];
+                int buf_j = xy[1] + col2Block[i][1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                int buf_max = map[buf_i][buf_j];
+                buf_i = xy[0] + col2Block[i + 1][0];
+                buf_j = xy[1] + col2Block[i + 1][1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                buf_max += map[buf_i][buf_j];
+
+                max = Math.max(max, buf_max);
+            }
+            for (int i = 4; i < 7; i++) {
+                int buf_i = xy[0] + col2Block[i][0];
+                int buf_j = xy[1] + col2Block[i][1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                int buf_max = map[buf_i][buf_j];
+                buf_i = xy[0] + col2Block[i + 1][0];
+                buf_j = xy[1] + col2Block[i + 1][1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                buf_max += map[buf_i][buf_j];
+
+                max = Math.max(max, buf_max);
+            }
+            result = Math.max(result, colBlock + max);
+        }
+
+        // row- 3
+        max = 0;
+        if (xy[1] + 2 < M) {
+            rowBlock = map[xy[0]][xy[1]] + map[xy[0]][xy[1] + 1] + map[xy[0]][xy[1] + 2];
+            for (int[] m : row3Block) {
+                int buf_i = xy[0] + m[0];
+                int buf_j = xy[1] + m[1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                max = Math.max(max, map[buf_i][buf_j]);
+            }
+            result = Math.max(result, rowBlock + max);
+        }
+
+        // col - 3
+        max = 0;
+        if (xy[0] + 2 < N) {
+            colBlock = map[xy[0]][xy[1]] + map[xy[0] + 1][xy[1]] + map[xy[0] + 2][xy[1]];
+            for (int[] m : col3Block) {
+                int buf_i = xy[0] + m[0];
+                int buf_j = xy[1] + m[1];
+                if (buf_i < 0 || buf_i >= N || buf_j < 0 || buf_j >= M) continue;
+                max = Math.max(max, map[buf_i][buf_j]);
+            }
+            result = Math.max(result, colBlock + max);
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j =0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            for (int j =0; j < M; j++) {
+                playGame(new int[]{i, j});
+            }
+        }
+        System.out.println(result);
+    }
+}
