03a8917304679283d673f8de6d014d9e9d12e93e
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2025-02-03 20:12:33 +0900

    [Silver I] Title: 트럭, Time: 116 ms, Memory: 14876 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,44 +1,50 @@
+import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt(); //트럭의 수
-        int w = sc.nextInt(); //다리의 길이
-        int L = sc.nextInt(); //다리의 최대 하중
-
-        Queue<Integer> queue = new LinkedList<>();
-        Queue<Integer> bridge = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            queue.offer(sc.nextInt());
-        }
-
-        int time = 0;
-        int truck_w = 0;
-        int pass = 0;
-
-        while(pass != n) {
-            time++;
-            if (bridge.size() == w) {
-                int a = bridge.remove();
-                truck_w -= a;
-                if(a != 0)
-                    pass++;
-            }
-
-            if (!queue.isEmpty()) {
-                if (truck_w + queue.element() <= L) {
-                    bridge.offer(queue.element());
-                    truck_w += queue.remove();
-                } else {
-                    bridge.offer(0);
-                }
-            } else {
-                bridge.offer(0);
-            }
-        }
-        System.out.println(time);
-    }
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken()); // 다리 길이
+		int L = Integer.parseInt(st.nextToken()); // 최대 하중
+		
+		List<Integer> bridge = new ArrayList<>();
+		int onbridge = 0;
+		int trucks = 0;
+		int time = 0;
+		int first = 0;
+		st = new StringTokenizer(br.readLine());
+		
+		int[] truck = new int[N];
+		
+		for(int n = 0; n < N; n++) {
+			truck[n] = Integer.parseInt(st.nextToken());
+		}
+        
+		for(int n = 0; n < N; n++) {
+			int now = truck[n];
+			
+			// 앞에 트럭 내보내기 
+			if (bridge.size() >= W) {
+				onbridge -= bridge.get(first++);
+				trucks--;
+			}
+			// 트럭을 올릴 수 있다면
+			if (onbridge + now <= L && trucks +1 <= W) {
+				bridge.add(now);
+				onbridge += now;
+				trucks++;
+			}
+			else { // 트럭을 올릴 수 없다면 
+				bridge.add(0);
+				n--;
+			}
+		}
+		System.out.println(bridge.size() + W);
+	}
 }
+
+
