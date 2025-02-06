7b7cccee7ecf85185f7b2b9a6d5b4597ed69da71
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2025-02-06 23:13:26 +0900

    [Silver I] Title: 절댓값 힙, Time: 320 ms, Memory: 25792 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,34 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-            int abs1 = Math.abs(o1);
-            int abs2 = Math.abs(o2);
+        PriorityQueue<Integer> queue = new PriorityQueue<>(
+                Comparator.comparingInt((Integer x) -> Math.abs(x))
+                        .thenComparingInt(x -> x));
 
-            if(abs1 == abs2) 
-                return o1 > o2 ? 1 : -1;
-            
-            return abs1 - abs2;
-        });
-
-        for(int i = 0 ; i < n; i++){
-            int a = Integer.parseInt(br.readLine());
-            
-            if(a == 0){
-                if(queue.isEmpty()) 
-                    System.out.println("0");
-                else 
-                    System.out.println(queue.poll());
+        StringBuilder sb = new StringBuilder();
+        for(int n = 0; n < N; n++) {
+            int now = Integer.parseInt(br.readLine());
+            if (now == 0) {
+                if (queue.isEmpty()) sb.append(0).append("\n");
+                else sb.append(queue.remove()).append("\n");
             }
-            else
-                queue.add(a);
+            else queue.add(now);
         }
+
+        System.out.println(sb);
     }
 }
