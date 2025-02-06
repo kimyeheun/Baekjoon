93837381208ef26eeb28b1285c72de874ce2028c
kimyeheun <81765156+kimyeheun@users.noreply.github.com>
2025-02-06 22:55:41 +0900

    [Silver II] Title: 최대 힙, Time: 284 ms, Memory: 26440 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,22 +1,22 @@
+import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int i = 0; i < test; i++) {
-            int a = sc.nextInt();
-            if(a == 0) {
-                if(!pq.isEmpty())
-                    System.out.println(pq.remove());
-                else
-                    System.out.println(0);
-            }
-            else
-                pq.add(a);
-        }
-
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+		StringBuilder sb = new StringBuilder();
+		for(int n = 0; n < N; n++) {
+			int now = Integer.parseInt(br.readLine());
+			if (now == 0) {
+				if (queue.isEmpty()) sb.append(0);
+				else sb.append(queue.poll());
+				sb.append("\n");
+			}
+			else queue.add(now);
+		}
+		System.out.println(sb);
+	}
 }
